e77821b1a4aacf35db9a327bbdf71ef049070db0
zeynal04 <zeynalmehdi123@gmail,com>
2024-08-01 23:38:55 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+
+// // task-1
+
+// for (let i = 1; i <= 5; i++) {
+//   let line = '';
+//   for (let j = 1; j <= i; j++) {
+//       line += '*';
+//   }
+//   console.log(line);
+// }
+
+
+
+
+// // task-2
+
+// for (let i = 5; i >= 1; i--) {
+//   let line = '';
+//   for (let j = 1; j <= i; j++) {
+//       line += '*';
+//   }
+//   console.log(line);
+// }
+
+
+
+// // task-3
+
+// function toplama(a, b) {
+//   console.log(a + b);
+// }
+
+
+// toplama(5, 3);    toplama(7,3);
+
+
+// function cixma(a, b) {
+//   console.log(a - b);
+// }
+
+
+// cixma(5, 3);    cixma(7,3);
+
+
+// function bolme(a, b) {
+//   console.log(a / b);
+// }
+
+
+// bolme(8, 2);    bolme(7,3);
+
+
+
+// function vurma(a, b) {
+//   console.log(a * b);
+// }
+
+
+// vurma(8, 2);    vurma(7,3);
